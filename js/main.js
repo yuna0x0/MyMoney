ba6e5d46358e285669e3e32eec17ae6789de14eb
Ming Chien Lee <edisonlee55@dumbeartech.com>
2016-07-13 15:41:00 +0800

    Update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
+var fdb = new ForerunnerDB();
+var db = fdb.db("MyAppDB");
+var collection = db.collection("MyAppCollection");
+collection.load();
 $("#submit").click(function() {
+    var date = $("#date").val();
     var name = $("#name").val();
     var category = $("#category").val();
     var price = $("#price").val();
-    if (name === "") {
+    if(date === ""){
+        alert("錯誤:日期不可為空白!");
+    }else if (name === "") {
         alert("錯誤:名稱不可為空白!");
     }else if(price === ""){
         alert("錯誤:價錢不可為空白!");
+    }else if($.isNumeric(price) !== true ){
+        alert("錯誤:價錢只可為數字!");
     }else{
-        console.log("OK!");
+        collection.insert({
+            date: date,
+            name: name,
+            category: category,
+            price: price
+        });
+        collection.save();
     }
 });
