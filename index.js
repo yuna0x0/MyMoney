46ccfe2524f003d4d63ee772ef606958d45990bd
Edison Lee <edisonlee@edisonlee55.com>
2017-10-19 14:40:42 +0800

    Update

---
+++ index.js
@@ -0,0 +1,6 @@
+/*!
+ * MyMoney - index.js v.1.0
+ * Copyright 2017 Edison Lee (MING-CHIEN LEE)
+ */
+console.log('MyMoney v.1.0.5');
+console.log('Copyright (C) 2017 MING-CHIEN LEE. All rights reserved.\n');
